14a0cb127032da10fe94da178ffbb3a694995628
keyur1363 <keyur1363@gmail.com>
2017-07-07 16:04:03 -0700

    btn
    btn

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-	
-	$(document).ready(function () {
+	(function (window, $) {
+	'use strict';
+	var path = window.location.pathname;
+	// Cache document for fast access.
+	var document = window.document;
 
 
 	$('#d-resume1').click(function () {
+
 	    window.location.href= window.location.protocol + "//" + window.location.host +path+"KEYUR_PATEL_RESUME.pdf";
 	});
 });
